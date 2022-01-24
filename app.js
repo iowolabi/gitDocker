a7b0e9785ba98d68ae5dc5920ac5b0f4e738ed08
Isaiah Owolabi <72822651+iowolabi@users.noreply.github.com>
2022-01-24 15:55:10 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+@@ -12,7 +12,7 @@ app.set('port', process.env.PORT || 7878);
+ var cmd=require('node-cmd');
+
+ app.post('/gitPush', function (req, res) {
+ console.log("Received the message inside gitPush->"+ JSON.stringify(req.body));
+ console.log("Received the message(from Git) inside gitPush->"+ JSON.stringify(req.body));
+
+     cmd.get('bash script.sh',
+         function(err, data, stderr){
+ @@ -32,4 +32,4 @@ console.log("Received the message inside gitPush->"+ JSON.stringify(req.body));
+
+
+
+ app.listen(app.get('port')); 
+ app.listen(app.get('port'));
+       
